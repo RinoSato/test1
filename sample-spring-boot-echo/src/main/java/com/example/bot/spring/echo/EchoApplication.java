96ce53a922d12878c2d643fe197859338f3f07c7
RinoSato <Rino.Sato@cypress.com>
2017-06-06 14:42:19 +0900

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -44,9 +44,4 @@ public class EchoApplication {
         System.out.println("event: " + event);
     }
     
-    public Message handleBeaconMessageEvent(BeaconEvent event) {
-    //System.out.println(event.getBeacon().getHwid()); //HWIDを標準出力
-    return new TextMessage("LINE Beanconから通知だよ！砂金さん、ありがとうございました！");
-    }
-
 }
